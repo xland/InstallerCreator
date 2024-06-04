3e8c43a216690e0b2b386e1d593ec1c51d1f8bcb
xland <412588801@qq.com>
2024-06-04 10:23:43 +0800

    main

---
+++ Installer/App.cpp
@@ -0,0 +1,49 @@
+﻿#include "App.h"
+#include <Windows.h>
+#include "include/core/SkGraphics.h"
+#include "include/core/SkFont.h"
+#include "include/core/SkFontMgr.h"
+#include "include/core/SkFontStyle.h"
+#include "include/ports/SkTypeface_win.h"
+
+
+namespace App {
+    sk_sp<SkFontMgr> fontMgr;
+    std::shared_ptr<SkFont> fontIcon{ nullptr };
+    std::shared_ptr<SkFont> fontText{ nullptr };
+
+	void Init()
+	{
+        SkGraphics::Init();
+        HRESULT hr = CoInitializeEx(NULL, COINIT_APARTMENTTHREADED | COINIT_DISABLE_OLE1DDE);
+        if (FAILED(hr)) return;
+        fontMgr = SkFontMgr_New_GDI();
+        fontText = std::make_shared<SkFont>(fontMgr->matchFamilyStyle("Microsoft YaHei", {}));
+        initFontIcon();
+        auto win = std::make_shared<WindowMain>();
+        win->show();
+        windows.push_back(std::move(win));
+        return true;
+	}
+
+    void App::initFontIcon()
+    {
+        HMODULE instance = GetModuleHandle(NULL);
+        HRSRC resID = FindResource(instance, MAKEINTRESOURCE(IDR_ICON_FONT), L"ICON_FONT");
+        if (resID == 0)
+        {
+            MessageBox(NULL, L"查找字体图标资源失败", L"系统提示", NULL);
+            return;
+        }
+        size_t resSize = SizeofResource(instance, resID);
+        HGLOBAL res = LoadResource(instance, resID);
+        if (res == 0)
+        {
+            MessageBox(NULL, L"加载字体图标资源失败", L"系统提示", NULL);
+            return;
+        }
+        LPVOID resData = LockResource(res);
+        auto fontData = SkData::MakeWithoutCopy(resData, resSize);
+        fontIcon = std::make_shared<SkFont>(fontMgr->makeFromData(fontData));
+    }
+}
---
+++ Installer/main.cpp
@@ -0,0 +1,17 @@
+#include <Windows.h>
+#include "App.h"
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPTSTR lpCmdLine, _In_ int nCmdShow)
+{
+    if (!App::init(hInstance)) {
+        return -1;
+    }
+    MSG msg;
+    while (GetMessage(&msg, NULL, 0, 0))
+    {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+    App::Dispose();
+    return 0;
+}
